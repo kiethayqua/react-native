9315e435113ea6890f9bcdb3056cd6d52ba6859f
kiethayqua <kiethayqua@gmail.com>
2019-11-02 15:24:30 +0700

    khai niem state va cach khoi tao va su dung

--- App.js
+++ App.js
@@ -2,20 +2,32 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import HinhCN from './components/HinhCN.js';
 
-// TouchableOpacity tuong tac voi nguoi dung
-// bao the TouchableOpacity ben ngoai khoi muon xu li cham man hinh
-// Dung thuoc tinh onPress={()={}} xu li su kien cham vao man hinh
+// STATE de cap nhat trang thai ngay khi thuc hien hanh dong
+// khoi tao trong constructor
+// this.state de khoi tao cac STATE
+// this.setState de cai dat cac state
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      so: 999
+    }
+  }
+
   showClick(){
-    console.log('CLICKED!')
+    console.log('CLICKED!');
+    this.setState({
+      so: this.state.so + 1
+    });
   }
 
   render(){
     return (
       <View>
+        <Text style={{fontSize: 100}}>{this.state.so}</Text>
         <TouchableOpacity onPress={()=>{this.showClick()}}>
-          <View style={{backgroundColor: 'green', width: 200, height: 50, marginTop: 30}}>
-            <Text>Click me</Text>
+          <View style={{backgroundColor: 'red', width: 50, height: 20}}>
+            <Text>+</Text>
           </View>
         </TouchableOpacity>
       </View>
